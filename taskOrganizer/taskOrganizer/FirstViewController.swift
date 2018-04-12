79be73764b0707b661fbcf8534ff983813c916d1
surnan <forsuresh@yandex.com>
2018-04-11 23:40:46 -0400

    NavigationController Setup along with left & right bar button items

--- taskOrganizer/taskOrganizer/FirstViewController.swift
+++ taskOrganizer/taskOrganizer/FirstViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    override func viewDidLoad() {
-        view.backgroundColor = UIColor.yellow
+    
+    func setupNavigationBar(){
+        self.navigationItem.title = "HELLO WORLD"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "*RIGHT*", style: .plain, target: self, action: #selector(handleRight))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "*LEFT*", style: .plain, target: self, action: #selector(handleLeft))
     }
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+        setupNavigationBar()
+
+    }
+    
+    
+    @objc func handleRight(){
+        print("click**")
+    }
+
+    @objc func handleLeft(){
+        print("**click")
+    }
+
+    
     
 }
