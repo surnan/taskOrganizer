342d030f396772fe99bfbe985163df2b84e08164
surnan <forsuresh@yandex.com>
2018-04-11 22:40:11 -0400

    successfully starts without storyboard

---
+++ taskOrganizer/taskOrganizer/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  taskOrganizer
+//
+//  Created by admin on 4/11/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.yellow
+    }
+    
+}
